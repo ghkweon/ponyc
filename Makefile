492ae7196d5e4e63a722dbaff8613effd35a740e
Gordon Tisher <gordon@balafon.net>
2019-06-27 11:20:47 -0700

    Build PonyC using CMake
    There are two stages, making the required libraries (vendored LLVM, GoogleTest, and GoogleBenchmark).
    
    There is a simple Makefile included that will build things in `build/release` and `build/debug` as before, so the tests can find the standard library.
    
    On Unix, run `make libs` to build the libraries and install them in `build/libs`.
    On Windows, run `./make.ps1 libs`.
    
    Then run `make configure` to run CMake's configuration.
    Then run `make build` to build PonyC.
    
    You can run `make test` to run a minimal set of tests, and `make test-ci` to run more tests and build examples.
    
    Fixes #3204
    
    Thanks to @plprofetes for their contributions.

--- Makefile
+++ Makefile
@@ -1,1 +1,72 @@
-Makefile-ponyc
+config ?= release
+arch ?= native
+version ?= $(shell cat VERSION)
+build_flags ?= -j2
+llvm_archs ?= X86
+llvm_config ?= Release
+
+srcDir := $(shell dirname '$(subst /Volumes/Macintosh HD/,/,$(realpath $(lastword $(MAKEFILE_LIST))))')
+buildDir := $(srcDir)/build/build_$(config)
+outDir := $(srcDir)/build/$(config)
+
+libsSrcDir := $(srcDir)/lib
+libsBuildDir := $(srcDir)/build/build_libs
+libsOutDir := $(srcDir)/build/libs
+
+ifndef verbose
+	SILENT = @
+else
+	SILENT =
+endif
+
+.DEFAULT_GOAL := build
+.PHONY: all libs cleanlibs configure build test test-ci test-check-version test-core test-stdlib-debug test-stdlib-release test-examples test-validate-grammar clean
+
+libs:
+	$(SILENT)mkdir -p '$(libsBuildDir)'
+	$(SILENT)cd '$(libsBuildDir)' && cmake -B '$(libsBuildDir)' -S '$(libsSrcDir)' -DCMAKE_INSTALL_PREFIX="$(libsOutDir)" -DCMAKE_BUILD_TYPE="$(llvm_config)" -DLLVM_TARGETS_TO_BUILD="$(llvm_archs)" -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_ENABLE_WARNINGS=OFF -DLLVM_ENABLE_TERMINFO=OFF
+	$(SILENT)cd '$(libsBuildDir)' && cmake --build '$(libsBuildDir)' --target install --config $(llvm_config) -- $(build_flags)
+
+cleanlibs:
+	$(SILENT)rm -rf '$(libsBuildDir)'
+	$(SILENT)rm -rf '$(libsOutDir)'
+
+configure:
+	$(SILENT)mkdir -p '$(buildDir)'
+	$(SILENT)cd '$(buildDir)' && cmake -B '$(buildDir)' -S '$(srcDir)' -DCMAKE_BUILD_TYPE=$(config) -DCMAKE_C_FLAGS="-march=$(arch)" -DCMAKE_CXX_FLAGS="-march=$(arch)" -DPONYC_VERSION=$(version)
+
+all: build
+
+build:
+	$(SILENT)cd '$(buildDir)' && cmake --build '$(buildDir)' --config $(config) --target all -- $(build_flags)
+
+test: all test-core test-stdlib-release test-examples
+
+test-ci: all test-check-version test-core test-stdlib-debug test-stdlib-release test-examples test-validate-grammar
+
+test-check-version: all
+	$(SILENT)cd '$(outDir)' && ./ponyc --version
+
+test-core: all
+	$(SILENT)cd '$(outDir)' && ./libponyrt.tests --gtest_shuffle
+	$(SILENT)cd '$(outDir)' && ./libponyc.tests --gtest_shuffle
+
+test-stdlib-release: all
+	$(SILENT)cd '$(outDir)' && ./ponyc -b stdlib-release --pic --checktree --verify ../../packages/stdlib && ./stdlib-release && rm stdlib-release
+
+test-stdlib-debug: all
+	$(SILENT)cd '$(outDir)' && ./ponyc -d -b stdlib-debug --pic --strip --checktree --verify ../../packages/stdlib && ./stdlib-debug && rm stdlib-debug
+
+test-examples: all
+	$(SILENT)cd '$(outDir)' && PONYPATH=.:$(PONYPATH) find ../../examples/*/* -name '*.pony' -print | xargs -n 1 dirname | sort -u | grep -v ffi- | xargs -n 1 -I {} ./ponyc -d -s --checktree -o {} {}
+
+test-validate-grammar: all
+	$(SILENT)cd '$(outDir)' && ./ponyc --antlr >> pony.g.new && diff ../../pony.g pony.g.new && rm pony.g.new
+
+clean:
+	$(SILENT)([ -d '$(buildDir)' ] && cd '$(buildDir)' && cmake --build '$(buildDir)' --config $(config) --target clean) || true
+	$(SILENT)rm -rf '$(buildDir)'
+	$(SILENT)rm -rf '$(outDir)'
+
+distclean:
+	$(SILENT)([ -d build ] && rm -rf build) || true
---
+++ make.ps1
@@ -0,0 +1,298 @@
+﻿Param(
+    [Parameter(Position=0, Mandatory=$true, HelpMessage="Enter the action to take, e.g. libs, cleanlibs, configure, build, clean, distclean, test, install.")]
+    [string]
+    $Command,
+
+    [Parameter(HelpMessage="The build configuration (Release, Debug, RelWithDebInfo, MinSizeRel).")]
+    [string]
+    $Config = "Release",
+
+    [Parameter(HelpMessage="The CMake generator, e.g. `"Visual Studio 16 2019`"")]
+    [string]
+    $Generator = "default",
+
+    [Parameter(HelpMessage="The architecture to use for compiling, e.g. `"x64`"")]
+    [string]
+    $Architecture = "x64",
+
+    [Parameter(HelpMessage="The location to install to")]
+    [string]
+    $InstallPath = "default",
+
+    [Parameter(HelpMessage="The version to use when packaging")]
+    [string]
+    $Version = "default"
+)
+
+# Sanitize config to conform to CMake build configs.
+switch ($Config.ToLower())
+{
+    "release" { $Config = "Release"; break; }
+    "debug" { $Config = "Debug"; break; }
+    "relwithdebinfo" { $Config = "RelWithDebInfo"; break; }
+    "minsizerel" { $Config = "MinSizeRel"; break; }
+    default { throw "'$Config' is not a valid config; use Release, Debug, RelWithDebInfo, or MinSizeRel)." }
+}
+$config_lower = $Config.ToLower()
+
+if ($null -eq (Get-Command "cmake.exe" -ErrorAction SilentlyContinue)) {
+	Write-Output "Warning, unable to find cmake.exe in your PATH, trying to discover one in Visual Studio installation."
+	Push-Location
+	$vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+	if (Test-Path $vswhere -PathType Leaf ) {
+    	$cmakePath = $(get-item $( Invoke-Expression '& "$vswhere" -latest -requires Microsoft.VisualStudio.Component.VC.CMake.Project -find Common7/IDE/**/cmake.exe ' )).Directory.FullName
+	    if ($null -ne $cmakePath) {
+			$env:Path = "$env:Path;$cmakePath"
+			Write-Output "Success, CMake added to current PATH from $cmakePath"
+		} else {
+			Write-Output "Your latest Visual Studio installation does not include CMake package."
+		}
+	} else {
+		Write-Output "No Visual Studio 2017+ was found in the system."
+	}
+	Pop-Location
+}
+
+
+if ($Generator -eq "default")
+{
+    $Generator = cmake --help | Where-Object { $_ -match '\*\s+(.*\S)\s+(\[arch\])?\s+=' } | Foreach-Object { $Matches[1].Trim() } | Select-Object -First 1
+}
+
+$srcDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+if ($Generator -match 'Visual Studio')
+{
+    $buildDir = Join-Path -Path $srcDir -ChildPath "build\build"
+}
+else
+{
+    $buildDir = Join-Path -Path $srcDir -ChildPath "build\build_$config_lower"
+}
+
+$libsDir = Join-Path -Path $srcDir -ChildPath "build\libs"
+$outDir = Join-Path -Path $srcDir -ChildPath "build\$config_lower"
+
+Write-Output "Source directory: $srcDir"
+Write-Output "Build directory:  $buildDir"
+Write-Output "Libs directory:   $libsDir"
+Write-Output "Output directory: $outDir"
+
+if ($InstallPath -eq "default")
+{
+    $InstallPath = Join-Path -Path $srcDir -ChildPath "build\install\$config_lower"
+}
+elseif (![System.IO.Path]::IsPathRooted($InstallPath))
+{
+    $InstallPath = Join-Path -Path $srcDir -ChildPath $InstallPath
+}
+
+Write-Output "make.ps1 $Command -Config $Config -Generator `"$Generator`" -InstallPath `"$InstallPath`""
+
+if (($Command.ToLower() -ne "libs") -and ($Command.ToLower() -ne "distclean") -and !(Test-Path -Path $libsDir))
+{
+    throw "Libs directory '$libsDir' does not exist; you may need to run 'make.ps1 libs' first."
+}
+
+if ($Generator.Contains("Win64") -or $Generator.Contains("Win32"))
+{
+    $Architecture = ""
+}
+
+switch ($Command.ToLower())
+{
+    "dummy" { break }
+    "libs"
+    {
+        if (!(Test-Path -Path $libsDir))
+        {
+            New-Item -ItemType "directory" -Path $libsDir | Out-Null
+        }
+
+        $libsBuildDir = Join-Path -Path $srcDir -ChildPath "build\build_libs"
+        if (!(Test-Path -Path $libsBuildDir))
+        {
+            New-Item -ItemType "directory" -Path $libsBuildDir | Out-Null
+        }
+
+        $libsSrcDir = Join-Path -Path $srcDir -ChildPath "lib"
+        Write-Output "Configuring libraries..."
+        if ($Architecture.Length -gt 0)
+        {
+            Write-Output "cmake.exe -B `"$libsBuildDir`" -S `"$libsSrcDir`" -G `"$Generator`" -A $Architecture -Thost=x64 -DCMAKE_INSTALL_PREFIX=`"$libsDir`" -DCMAKE_BUILD_TYPE=Release" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_ENABLE_WARNINGS=OFF -DLLVM_ENABLE_TERMINFO=OFF
+            & cmake.exe -B "$libsBuildDir" -S "$libsSrcDir" -G "$Generator" -A $Architecture -Thost=x64 -DCMAKE_INSTALL_PREFIX="$libsDir" -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_ENABLE_WARNINGS=OFF -DLLVM_ENABLE_TERMINFO=OFF
+        }
+        else
+        {
+            Write-Output "cmake.exe -B `"$libsBuildDir`" -S `"$libsSrcDir`" -G `"$Generator`" -Thost=x64 -DCMAKE_INSTALL_PREFIX=`"$libsDir`" -DCMAKE_BUILD_TYPE=Release" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_ENABLE_WARNINGS=OFF -DLLVM_ENABLE_TERMINFO=OFF
+            & cmake.exe -B "$libsBuildDir" -S "$libsSrcDir" -G "$Generator" -Thost=x64 -DCMAKE_INSTALL_PREFIX="$libsDir" -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_ENABLE_WARNINGS=OFF -DLLVM_ENABLE_TERMINFO=OFF
+        }
+        if (!$?) { throw "Error: exit code $LastExitCode" }
+
+        Write-Output "Building libraries..."
+        Write-Output "cmake.exe --build `"$libsBuildDir`" --target install --config Release"
+        & cmake.exe --build "$libsBuildDir" --target install --config Release
+        if (!$?) { throw "Error: exit code $LastExitCode" }
+        break
+    }
+    "cleanlibs"
+    {
+        if (Test-Path -Path $libsDir)
+        {
+            Write-Output "Removing $libsDir..."
+            Remove-Item -Path $libsDir -Recurse
+        }
+        break
+    }
+    "configure"
+    {
+        if ($Architecture.Length -gt 0)
+        {
+            Write-Output "cmake.exe -B `"$buildDir`" -S `"$srcDir`" -G `"$Generator`" -A $Architecture -Thost=x64 -DCMAKE_INSTALL_PREFIX="$InstallPath" -DCMAKE_BUILD_TYPE=`"$Config`""
+            & cmake.exe -B "$buildDir" -S "$srcDir" -G "$Generator" -A $Architecture -Thost=x64 -DCMAKE_INSTALL_PREFIX="$InstallPath" -DCMAKE_BUILD_TYPE="$Config" --no-warn-unused-cli
+        }
+        else
+        {
+            Write-Output "cmake.exe -B `"$buildDir`" -S `"$srcDir`" -G `"$Generator`" -Thost=x64 -DCMAKE_INSTALL_PREFIX="$InstallPath" -DCMAKE_BUILD_TYPE=`"$Config`""
+            & cmake.exe -B "$buildDir" -S "$srcDir" -G "$Generator" -Thost=x64 -DCMAKE_INSTALL_PREFIX="$InstallPath" -DCMAKE_BUILD_TYPE="$Config" --no-warn-unused-cli
+        }
+        if (!$?) { throw "Error: exit code $LastExitCode" }
+        break
+    }
+    "build"
+    {
+        Write-Output "cmake.exe --build `"$buildDir`" --config $Config --target ALL_BUILD"
+        & cmake.exe --build "$buildDir" --config $Config --target ALL_BUILD
+        if (!$?) { throw "Error: exit code $LastExitCode" }
+        break
+    }
+    "clean"
+    {
+        Write-Output "cmake.exe --build `"buildDir`" --config $Config --target clean"
+        & cmake.exe --build "$buildDir" --config $Config --target clean
+
+        if (Test-Path $outDir)
+        {
+            Write-Output "Remove-Item -Path $outDir -Recurse -Force"
+            Remove-Item -Path "$outDir" -Recurse -Force
+        }
+        break
+    }
+    "distclean"
+    {
+        if (Test-Path ($srcDir + "\build"))
+        {
+            Write-Output "Remove-Item -Path `"$srcDir\build`" -Recurse -Force"
+            Remove-Item -Path "$srcDir\build" -Recurse -Force
+        }
+        break
+    }
+    "test"
+    {
+        $numTestSuitesRun = 0
+        $failedTestSuites = @()
+
+        & $outDir\ponyc.exe --version
+
+        # libponyrt.tests
+        $numTestSuitesRun += 1;
+        Write-Output "$outDir\libponyrt.tests.exe --gtest_shuffle"
+        & $outDir\libponyrt.tests.exe --gtest_shuffle
+        if (!$?) { $failedTestSuites += 'libponyrt.tests' }
+
+        # libponyc.tests
+        $numTestSuitesRun += 1;
+        Write-Output "$outDir\libponyc.tests.exe --gtest_shuffle"
+        & $outDir\libponyc.tests.exe --gtest_shuffle
+        if (!$?) { $failedTestSuites += 'libponyc.tests' }
+
+        # stdlib-debug
+        $numTestSuitesRun += 1;
+        Write-Output "$outDir\ponyc.exe -d --checktree --verify -b stdlib-debug -o $outDir $srcDir\packages\stdlib"
+        & $outDir\ponyc.exe -d --checktree --verify -b stdlib-debug -o $outDir $srcDir\packages\stdlib
+        if ($LastExitCode -eq 0)
+        {
+            Write-Output "$outDir\stdlib-debug.exe"
+            & $outDir\stdlib-debug.exe --exclude="net/Broadcast"
+            if (!$?) { $failedTestSuites += 'stdlib-debug' }
+        }
+        else
+        {
+            $failedTestSuites += 'compile stdlib-debug'
+        }
+
+        # stdlib-release
+        $numTestSuitesRun += 1;
+        Write-Output "$outDir\ponyc.exe --checktree --verify -b stdlib-release -o $outDir $srcDir\packages\stdlib"
+        & $outDir\ponyc.exe --checktree --verify -b stdlib-release -o $outDir $srcDir\packages\stdlib
+        if ($LastExitCode -eq 0)
+        {
+            Write-Output "$outDir\stdlib-release.exe"
+            & $outDir\stdlib-release.exe --exclude="net/Broadcast"
+            if (!$?) { $failedTestSuites += 'stdlib-release' }
+        }
+        else
+        {
+            $failedTestSuites += 'compile stdlib-release'
+        }
+
+        # grammar
+        $numTestSuitesRun += 1
+        Get-Content -Path "$srcDir\pony.g" -Encoding ASCII | Out-File -Encoding UTF8 "$outDir\pony.g.orig"
+        & $outDir\ponyc.exe --antlr | Out-File -Encoding UTF8 "$outDir\pony.g.test"
+        if ($LastExitCode -eq 0)
+        {
+            $origHash = (Get-FileHash -Path "$outDir\pony.g.orig").Hash
+            $testHash = (Get-FileHash -Path "$outDir\pony.g.test").Hash
+
+            Write-Output "grammar original hash:  $origHash"
+            Write-Output "grammar generated hash: $testHash"
+
+            if ($origHash -ne $testHash)
+            {
+                $failedTestSuites += 'generated grammar file differs from baseline'
+            }
+        }
+        else
+        {
+            $failedTestSuites += 'generate grammar'
+        }
+
+        #
+        $numTestSuitesFailed = $failedTestSuites.Length
+        Write-Output "Test suites run: $numTestSuitesRun, num failed: $numTestSuitesFailed"
+        if ($numTestSuitesFailed -ne 0)
+        {
+            $failedTestSuitesList = [string]::Join(', ', $failedTestSuites)
+            Write-Output "Test suites failed: ($failedTestSuitesList)"
+            exit $numTestSuitesFailed
+        }
+
+        break
+    }
+    "install"
+    {
+        Write-Output "cmake.exe --build `"$buildDir`" --config $Config --target install"
+        & cmake.exe --build "$buildDir" --config $Config --target install
+        if (!$?) { throw "Error: exit code $LastExitCode" }
+
+        break
+    }
+    "package"
+    {
+        switch ($Version)
+        {
+            "default" { $Version = (Get-Content $srcDir\VERSION) + "-" + (git rev-parse --short --verify HEAD^) }
+            "date" { $Version = (Get-Date).ToString("yyyyMMdd") }
+        }
+
+        $package = "ponyc-x86_64-pc-windows-msvc-$Version-$Config.zip"
+        Write-Output "Creating $buildDir\$package"
+
+        Compress-Archive -Path "$InstallPath\ponyc", "$InstallPath\packages", "$InstallPath\examples" -DestinationPath "$buildDir\$package" -Force
+    }
+    default
+    {
+        throw "Unknown command '$Command'; use: {libs, cleanlibs, configure, build, clean, distclean, test, install}"
+    }
+}
